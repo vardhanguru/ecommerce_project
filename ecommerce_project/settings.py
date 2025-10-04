48a8a2a391ac25f341ce02146fc49c9e9b81e74e
vardhanguru <68608658+vardhanguru@users.noreply.github.com>
2025-10-04 09:47:27 +0530

    Update settings.py

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -78,11 +78,12 @@ WSGI_APPLICATION = 'ecommerce_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://demo2_0fmo_user:Lwn73LiSmwKzgXOGcK52gWVpjW5RdNwS@dpg-d3g9td7fte5s73bul9gg-a.oregon-postgres.render.com/demo2_0fmo',
+        conn_max_age=600,  # keeps DB connections alive (good for production)
+    )
 }
+
 
 
 # Password validation
